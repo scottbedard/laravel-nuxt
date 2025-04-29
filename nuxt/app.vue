3f256348e2b71d2ea83ade930602e6e88e981a6a
Scott Bedard <scottbedard90@gmail.com>
2025-04-28 19:38:07 -0500

    wip

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <pre>{{ { user } }}</pre>
+
+    <form @submit.prevent="onRegister">
+      <div>Register</div>
+      <input type="email" v-model="register.email" />
+      <input type="password" v-model="register.password" />
+      <button type="submit">Register</button>
+    </form>
   </div>
 </template>
+
+<script lang="ts" setup>
+const auth = useSanctumAuth();
+
+const client = useSanctumClient();
+
+const user = useSanctumUser();
+
+const register = ref({
+  email: 'alice@example.com',
+  password: 'password',
+})
+
+async function onRegister() {
+  await client('http://localhost:80/api/register', {
+    method: 'POST',
+    body: register.value,
+  })
+}
+</script>
