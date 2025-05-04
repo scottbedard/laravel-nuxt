eb01794422e5bf4e82c69eab62b607760121fad2
Scott Bedard <scottbedard90@gmail.com>
2025-05-04 11:31:55 -0500

    wip

--- tests/Playwright/specs/example.spec.ts
+++ tests/Playwright/specs/example.spec.ts
@@ -2,7 +2,9 @@ import { expect, test } from 'playwright/index'
 
 test.describe('example', () => {
   test('home page', async ({ app, page }) => {
-    await app.auth()
+    const user = await app.auth()
+
+    console.log({ user })
 
     await page.goto('/')
     
