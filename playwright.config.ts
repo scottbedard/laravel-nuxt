0100d33e1ff1437a657d8ae7b359842fc5509bc8
Scott Bedard <scottbedard90@gmail.com>
2025-05-04 11:20:44 -0500

    playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://playwright.dev/docs/test-configuration
 export default defineConfig({
