cf9e480862fceee65c1cd03e7230114613466429
Scott Bedard <scottbedard90@gmail.com>
2025-05-03 23:59:26 -0500

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: [
+    '@/assets/css/tailwind.css',
+  ],
   devtools: {
     enabled: true,
   },
@@ -11,4 +16,7 @@ export default defineNuxtConfig({
     'nuxt-auth-sanctum',
   ],
   srcDir: 'nuxt',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,30 +1,94 @@
 <template>
   <div>
-    <pre>{{ { user } }}</pre>
+    <div v-if="user">
+      <h1 class="font-semibold tracking-wide">Current user</h1>
 
-    <form @submit.prevent="onRegister">
-      <div>Register</div>
-      <input type="email" v-model="register.email" />
-      <input type="password" v-model="register.password" />
-      <button type="submit">Register</button>
-    </form>
+      <pre class="my-3 text-sm">{{ user }}</pre>
 
-    <br>
+      <button
+        type="button"
+        class="cursor-pointer rounded-md bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+        @click="onLogout">
+        Logout
+      </button>
+    </div>
 
-    <form @submit.prevent="onLogin">
-      <div>Login</div>
-      <input type="email" v-model="login.email" />
-      <input type="password" v-model="login.password" />
-      <button type="submit">Login</button>
-    </form>
+    <div
+      v-else
+      class="flex flex-wrap gap-6">
+      <form
+        class="flex-1 gap-2 grid max-w-64"
+        @submit.prevent="onRegister">
+        <h2 class="font-semibold tracking-wide">Register</h2>
 
-    <br>
+        <input
+          v-model="register.email"
+          aria-label="Email" 
+          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          name="email" 
+          placeholder="you@example.com"
+          required
+          type="email" />
+        
+        <input
+          v-model="register.password"
+          aria-label="Password" 
+          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          name="password"
+          required
+          type="password" />
 
-    <button @click="onLogout">Logout</button>
+        <div class="flex">
+          <button
+            type="submit"
+            class="cursor-pointer rounded-md bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+            Register
+          </button>
+        </div>
+      </form>
+
+      <form
+        class="flex-1 gap-2 grid max-w-64"
+        @submit.prevent="onLogin">
+        <h2 class="font-semibold tracking-wide">Login</h2>
+
+        <input
+          v-model="login.email"
+          aria-label="Email" 
+          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          name="email" 
+          placeholder="you@example.com"
+          required
+          type="email" />
+        
+        <input
+          v-model="login.password"
+          aria-label="Password" 
+          class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          name="password"
+          required
+          type="password" />
+
+        <div class="flex">
+          <button
+            type="submit"
+            class="cursor-pointer rounded-md bg-indigo-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+useHead({
+  title: 'laravel-nuxt',
+  bodyAttrs: {
+    class: 'antialiased p-6',
+  },
+})
+
 const auth = useSanctumAuth();
 
 const client = useSanctumClient();
