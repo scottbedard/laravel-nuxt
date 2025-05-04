126294b41b0a44d603dbd78e8c588d32a60d8d63
Scott Bedard <scottbedard90@gmail.com>
2025-05-04 09:53:41 -0500

    scaffold testing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->to(config('app.frontend_url')));
+Route::get('/', function () {
+  return redirect()->to(config('app.frontend_url'));
+});
+
+Route::get('/health', function () {
+  return response()->json(['status' => 'ok']);
+});
 
 require __DIR__ . '/auth.php';
