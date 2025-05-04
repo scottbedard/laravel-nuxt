4664e24c132efd07e49af491a0a4348328477a09
Scott Bedard <scottbedard90@gmail.com>
2025-05-04 16:36:37 -0500

    cleanup

--- tests/Playwright/specs/example.spec.ts
+++ tests/Playwright/specs/example.spec.ts
@@ -1,19 +1,12 @@
 import { expect, test } from 'playwright/index'
-import fs from 'node:fs'
 
 test.describe('example', () => {
   test('home page', async ({ app, page }) => {
     await app.refreshDatabase()
 
     const user = await app.auth()
-    
-    const response = await page.goto('/')
-    const text = await response?.text()
-    const status = response?.status()
 
-    fs.writeFileSync('response-text.artifact', text ?? 'empty')
-
-    console.log({ text, status, user })
+    await page.goto('/')
     
     await expect(page).toHaveTitle('laravel-nuxt')
     await expect(page.getByTestId('user')).toContainText(user.email)
