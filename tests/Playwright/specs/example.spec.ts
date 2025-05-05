f6da61a531bcfc3982d267ae4e76748bfcdc767b
Scott Bedard <scottbedard90@gmail.com>
2025-05-04 22:53:41 -0500

    simplify

--- tests/Playwright/specs/example.spec.ts
+++ tests/Playwright/specs/example.spec.ts
@@ -2,13 +2,10 @@ import { expect, test } from  '#playwright/index'
 
 test.describe('example', () => {
   test('home page', async ({ app, page }) => {
-    await app.refreshDatabase()
-
     const user = await app.auth()
 
     await page.goto('/')
-    
-    await expect(page).toHaveTitle('laravel-nuxt')
+
     await expect(page.getByTestId('user')).toContainText(user.email)
   })
 })
