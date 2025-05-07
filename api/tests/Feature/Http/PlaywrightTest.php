b40bb9ccc245c8d4460073b3d58f9a9dbfd76bc9
Scott Bedard <scottbedard90@gmail.com>
2025-05-06 22:54:14 -0500

    tests

--- api/tests/Feature/Http/PlaywrightTest.php
+++ api/tests/Feature/Http/PlaywrightTest.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 test('csrf token', function () {
     $response = $this->get('/api/__playwright__/csrf_token');
 
     expect($response->json())->toEqual(csrf_token());
+});
+
+test('login', function () {
+    $alice = User::factory()->create();
+
+    $this->post('/api/__playwright__/login', [
+        'attributes' => [
+            'email' => $alice->email,
+        ],
+    ]);
+
+    expect(Auth::id())->toEqual($alice->id);
+});
+
+test('run php', function () {
+    $response = $this->post('/api/__playwright__/run-php', [
+        'command' => 'return 12345;',
+    ]);
+
+    expect($response->json('result'))->toEqual(12345);
 });
